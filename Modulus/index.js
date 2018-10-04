79efe421c743190ffd67ee96a745c741f7780cea
MeganKSpence <43014740+MeganKSpence@users.noreply.github.com>
2018-10-04 13:22:01 -0600

    update

--- Modulus/index.js
+++ Modulus/index.js
@@ -1,6 +1,9 @@
-let number = [17, 37, 1]; {
-  let startingNumber = number[0];
-  console.log(startingNumber);
-  let skipcount =;
-  let difference = number[1]-number[0];
+let number = [17, 27];
+let skipcount = 1;
+let difference = number[1] - number[0]
+let startingnumber = number[0];
+for (let i=0; i<=difference; i++){
+  even = (startingnumber += skipcount) % 2;
+  console.log("This number is", even, "; This iteration is number", i+skipcount, "the number is: ", );
 }
+if
