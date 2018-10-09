4bd7ce8eda089aa28a780407bcc8da3886d717fc
MeganKSpence <43014740+MeganKSpence@users.noreply.github.com>
2018-10-09 13:29:24 -0600

    update

--- Max of Two Numbers/index.js
+++ Max of Two Numbers/index.js
@@ -1,5 +1,12 @@
-let number = [4, 5];
-
-console.log(number);
-
-number[0] > number[1] ? "the first number is bigger" : "the second number is bigger";
+function biggerNumber (number1, number2){
+  let number = [4, 5];
+    if (number[0] > number[1]){
+      console.log("the bigger number is: " + number[0])
+    }
+    else if (number[0] < number[1]){
+      console.log("the bigger number is: " + number[1])
+    }
+    else if(number[0] === number[1]){
+      console.log("the numbers are equal")
+    }
+}
