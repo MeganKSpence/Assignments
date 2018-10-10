560052378a21ce22ca767d450a9fa70befc18e0e
MeganKSpence <43014740+MeganKSpence@users.noreply.github.com>
2018-10-09 21:04:34 -0600

    update

--- Greeting/index.js
+++ Greeting/index.js
@@ -1,5 +1,4 @@
-let hour = 24; //Change this simulating automatically reading time from Internet Clock or System Tray Clock
-24 === 0
+let hour = 24;
 if (hour < 12) console.log('Good Morning');
 else if (hour < 19) console.log('Good Afternoon');
-else console.log("Good Evening");
+else if (hour > 19) console.log('Good Evening');
