b5c64b9800878e19663f97c80adea2f0285eb25f
MeganKSpence <43014740+MeganKSpence@users.noreply.github.com>
2018-10-13 10:51:12 -0600

    update

--- Modulus/index.js
+++ Modulus/index.js
@@ -1,4 +1,4 @@
-let number = [18, 37];
+let number = [19, 37];
 console.log("The numbers being used are " + number[0] + " and " + number[1])
 let skipcount = 1;
 let difference = number[1] - number[0]
@@ -14,20 +14,24 @@ for (let i=0; i<=difference; i++){
   }
 }
 
-for (i=0; i<=difference; i++);
-  arrays = (number[0]+i) % 2;
-    if (arrays === 0){
-      console.log(arrays);
+console.log("evenArray") //note: Odd and Even Array do not Work Properly, Only Work when number[0] is even, I tried my best but could not figure it out
+for (let i=0; i<=difference; i++){
+  even = (startingnumber += skipcount) % 2;
+    if (even === 0){
+      "ignore"
+    }
+    else if (even === 1) {
+      console.log(number[0]+i);
+    }
 }
 
-//  console.log((even = 1) ? "This number is odd" : "This number is even");
-// console.log("This number is", even, "; This iteration is number", i+skipcount);
-
-//for (let i=0; i<=difference; i++){
-//  even = (startingnumber += skipcount) % 2;
-//  if (even === 0){
-//    let evenArray = [];
-//      evenArray = new Array(20);
-//        console.log(evenArray = [number[0]+i]);
-//  }
-//}
+console.log("oddArray")
+for (let i=0; i<=difference; i++){
+  even = (startingnumber += skipcount) % 2;
+    if (even === 0){
+      console.log(number[0]+i);
+    }
+    else if (even === 1) {
+      "ignore"
+    }
+}
